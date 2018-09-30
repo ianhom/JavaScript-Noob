359d92db2886f514ce210dd39b66217717c246fe
A Byte of China <ianhom@163.com>
2018-09-30 22:41:09 +0800

    Update page1.js

--- app/PersimmonUI/lesson5/page1.js
+++ app/PersimmonUI/lesson5/page1.js
@@ -1,10 +1,19 @@
 var data = [ 45, 30, 40, 50, 32, 56, 12, 34, 21, 23, 13, 46 ];  //柱形图数据
 var date = ['2018-09-29','2018-09-30','2018-10-01']
+var code = ['Sunny','Cloudy','Snowy']
 var high = [0, 0, 0]
 var low  = [0, 0, 0]
 var high1 = [25, 26, 24]
 var low1  = [15, 10, 17]
 var DISTANCE = 110
+var jsons = '{"results":[{"location":{"id":"WX4FBXXFKE4F","name":"Beijing","country":"CN","path":"Beijing,Beijing,China","timezone":"Asia/Shanghai","timezone_offset":"+08:00"},"daily":[{"date":"2018-09-30","text_day":"Cloudy","code_day":"4","text_night":"Cloudy","code_night":"4","high":"19","low":"13","precip":"","wind_direction":"NW","wind_direction_degree":"315","wind_speed":"20","wind_scale":"4"},{"date":"2018-10-01","text_day":"Sunny","code_day":"0","text_night":"Sunny","code_night":"1","high":"24","low":"12","precip":"","wind_direction":"N","wind_direction_degree":"0","wind_speed":"20","wind_scale":"4"},{"date":"2018-10-02","text_day":"Sunny","code_day":"0","text_night":"Sunny","code_night":"1","high":"24","low":"11","precip":"","wind_direction":"N","wind_direction_degree":"0","wind_speed":"10","wind_scale":"2"}],"last_update":"2018-09-30T11:00:00+08:00"}]}'
+
+var s1 = 'https://api.seniverse.com/v3/weather/daily.json?key=hfvxxp7oq0w4dmso&location='
+var city = 'beijing'
+var s2 = '&language=en&unit=c&start='
+var day = '0'
+var s3 = '&days=3'
+var url_w = s1+city+s2+day+s3
 
 var xx = 1;
 var xx1 = 1;
@@ -14,6 +23,7 @@ var page = {
     dataa : {timer1 : 0},
     onBtn : function()
     {
+        this.onUpdate()
         this.getTemp()
         for(var k = 0; k < high.length; k++)
         {
@@ -31,13 +41,13 @@ var page = {
             for(var k = 0; k < high.length; k++)
             {
                 if(high[k] <  high1[k]){
-                    high[k] += high1[k]/10
-                    console.log(high[k])}
+                    high[k] += Math.round(high1[k]/10)
+                }
                 if(low[k] < low1[k]){
-                    low[k]  += low1[k]/10
+                    low[k]  += Math.round(low1[k]/10)
                 }
             }
-            //thiz.getTemp();
+            
             thiz.display();
         }, 50);
         this.display()
@@ -52,18 +62,11 @@ var page = {
         }
     },
 
-  
-
     display : function()
     {
-        var index = 0
-        
-        
-        
         var context = pm.createCanvasContext('Canvas1', this)           //获取画布对象
 
-        
-        if (context)
+         if (context)
         {
             var max = 35;
 
@@ -71,15 +74,15 @@ var page = {
             {
                 grd = context.createLinearGradient(0, 0, 0, 480)
                 grd.addColorStop(0, 'black')
-                if(j == 0)
+                if(code[j] == 'Sunny')
                 {
-                    grd.addColorStop(1, 'BLUE')
-                }else if(j == 1)
+                    grd.addColorStop(1, '#FF9090FF')
+                }else if(code[j] == 'Cloudy')
                 {
                     grd.addColorStop(1, 'GRAY')
                 }else
                 {
-                    grd.addColorStop(1, 'GREEN')
+                    grd.addColorStop(1, '#FFB9FFB9')
                 }
                 // Fill with gradient
                 context.setFillStyle(grd)
@@ -152,7 +155,7 @@ var page = {
             context.moveTo(x, 380)
             context.beginPath();
             context.setLineCap('round')
-            context.setStrokeStyle('RED')
+            context.setStrokeStyle('#FFFF6969')
             context.setLineWidth(4)        //设置线条宽度          
             for( index = 0; index < high.length; index++ )
             {
@@ -170,7 +173,7 @@ var page = {
             var x = 60 + DISTANCE;
             context.moveTo(x, 380)
             context.beginPath();
-            context.setStrokeStyle('BLUE')
+            context.setStrokeStyle('#FF6969FF')
             //context.setLineWidth(2)        //设置线条宽度 
             for( index = 0; index < low.length; index++ )
             {
@@ -182,14 +185,59 @@ var page = {
             context.stroke()
             context.closePath();
             
-            
-            
-            
+            // Value
+            context.setTextAlign('center')
+            context.setFillStyle('white')
+            for( index = 0; index < 3; index++ )
+            {
+                var x = 60 + DISTANCE/2 + index * DISTANCE;
+                var y = high[index] * 350 / (max * 7)
+                context.fillText(high[index].toString(), x, 365-y)
+                
+                var x = 60 + DISTANCE/2 + index * DISTANCE;
+                var y = low[index] * 350 / (max * 7)
+                context.fillText(low[index].toString(), x, 395-y)
+                
+                var x = 60 + DISTANCE/2 + index * DISTANCE;
+                context.fillText(code[index], x, 50)
+            }
             
             context.draw();
                        
         }
-    }
+    },
+    onUpdate : function(){		//根据json_obj中的数据，来设定对应Label的值
+        //str = '{"sk_info":{"date":"20131012","cityName":"123","areaID":"101010100","temp":"21","tempF":"69.8","wd":"3","ws":"33","sd":"39%","time":"15:10","sm":"3"}}'
+		//json_obj = JSON.parse(str)
+		console.log(json_obj.results[0].daily[0].date)
+		//console.log(json_obj.results[0].daily[1].date)
+		//console.log(json_obj.results[0].daily[2].date)
+		//this.onSetDate(json_obj.sk_info.date);
+//		this.setData({temp : {value : json_obj.sk_info.temp , refresh : true}});
+//		this.setData({wind : {value : json_obj.sk_info.wd , refresh : true}});
+
+	},
+    
+    getSkyInfo: function(e) {
+	var thiz = this;
+	var str =0;
+	
+	var rq1 = pm.request({
+	    url: url_w, // 获取天气预报的API，返回伪数据
+		method : 'GET',
+		header:{
+			"Content-Type":"application/json"
+		},
+		success: function(res) {		//与开发者服务器连接成功后，执行的回调函数
+			str = res.data.toString('utf8');	// 把data从Buffer转成string
+			json_obj=JSON.parse(str);			//把JSON格式的string转成JSON对象，以便获取数据
+			thiz.onUpdate();	//更新各个控件的值
+		},
+		fail: function(){
+			console.log('request failed')
+		}
+		});
+	}
 };
 
 Page(page);
